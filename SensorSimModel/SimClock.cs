879c6e1d04b953acb53a5e973a9ed5f2d90d1696
FraChr <empynia@gmail.com>
2026-02-10 15:17:59 +0100

    Batman

---
+++ SensorSimModel/SimClock.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using SensorSimDependancies;
+
+namespace SensorSimModel;
+
+public class Clock : IClock
+{
+    private Stopwatch Stopwatch {get; set;} = new();
+    
+    public Stopwatch StartStopwatch()
+    {
+        return Stopwatch.StartNew();
+    }
+
+    public void StopStopwatch()
+    {
+        Stopwatch.Stop();
+    }
+    
+}
