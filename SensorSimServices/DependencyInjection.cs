8298697e260319a86c9cfd508a6cb570d23a7b1b
FraChr <empynia@gmail.com>
2026-02-12 15:09:33 +0100

    added new dependancies to di class

--- SensorSimServices/DependencyInjection.cs
+++ SensorSimServices/DependencyInjection.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using SensorSimDependancies;
+using SensorSimDependancies.LogicInterfaces;
 using SensorSimDependancies.ModelInterfaces;
 using SensorSimModel;
+using SensorSimModel.Environment;
+using SensorSimModel.Sensor;
 
 namespace SensorSimServices;
 
@@ -10,6 +13,8 @@ public static class DependencyInjection
     public static IServiceCollection AddAppServices(this IServiceCollection services)
     {
         services.AddSingleton<IClock, SimClock>();
+        services.AddSingleton<IOcean, Ocean>();
+        services.AddSingleton<ITemperature, TempSensor>();
         return services;
     }
 }
