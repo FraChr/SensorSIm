e4ea001dc56f99dd39a6559bf62a538a970c3329
FraChr <empynia@gmail.com>
2026-02-27 13:35:36 +0100

    sensor factory

--- SensorSimLogic/SensorFactory.cs
+++ SensorSimLogic/SensorFactory.cs
@@ -1,6 +1,36 @@
-﻿namespace SensorSimLogic;
+﻿using SensorSimDependancies.LogicInterfaces;
+using SensorSimModel.Sensor;
 
-public class SensorFactory
+namespace SensorSimLogic;
+
+public class SensorFactory : ISensorFactory
 {
+    private readonly Dictionary<string, Func<ISensorLogic>> _creators;
+
+    public SensorFactory()
+    {
+        _creators = new()
+        {
+            {"Temperature", () => new TempSensor()},
+            {"Pressure", () => new  PressureSensor()}
+        };
+    }
     
+    public ISensorLogic Create(string sensorType)
+    {
+        if (!_creators.TryGetValue(sensorType, out var creator))
+            throw new ArgumentException($"Sensor type '{sensorType}' not registered.");
+        
+        return creator();
+    }
+
+    public IEnumerable<ISensorDisplayModel> GetAvailableSensors()
+    {
+        /*return _creators.Keys;*/
+
+        return _creators.Keys.Select(key => new SensorDisplayModel(key)
+        {
+            Name = key,
+        });
+    }
 }
