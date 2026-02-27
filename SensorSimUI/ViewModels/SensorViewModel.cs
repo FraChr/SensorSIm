96bef76c6eec36a2e8fa9a8639a7ecf249ecc1a0
FraChr <empynia@gmail.com>
2026-02-27 13:40:42 +0100

    updated active sensors from list to observableCollection, also added avaliableSensors collected from factory setup also updated the refresh and setSensor mehtods to reflect overal changes in app

--- SensorSimUI/ViewModels/SensorViewModel.cs
+++ SensorSimUI/ViewModels/SensorViewModel.cs
@@ -1,7 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using SensorSimDependancies.LogicInterfaces;
-using SensorSimModel.Sensor;
 
 namespace SensorSimUI.ViewModels;
 
@@ -9,36 +9,56 @@ public sealed class SensorViewModel : INotifyPropertyChanged
 {
     public event PropertyChangedEventHandler? PropertyChanged;
     
-    private ISensorHandler ? _sensorHandler;
+    private readonly ISensorHandler? _sensorHandler;
     
-    private readonly ITemperature _temperature;
-    public string? TempName { get; set; }
-    
-    private List<ISensorDisplayModel> _sensors;
-    public List<ISensorDisplayModel> Sensors
+    private readonly ISensorFactory _sensorFactory;
+
+    public IEnumerable<ISensorDisplayModel> AvailableSensorTypes { get; }
+
+    private ObservableCollection<ISensorDisplayModel> _activeSensors = new();
+    public ObservableCollection<ISensorDisplayModel> ActiveSensors
     {
-        get => _sensors;
-        private set => SetField(ref _sensors, value);
+        get => _activeSensors;
+        private set => SetField(ref _activeSensors, value);
     }
-    
-    public SensorViewModel(ITemperature temperature, ISensorHandler sensorHandler)
+
+    public SensorViewModel(ISensorHandler sensorHandler, ISensorFactory sensorFactory)
     {
         _sensorHandler = sensorHandler;
-        /*RefreshSensors();*/
-        _temperature = temperature;
-        TempName = _temperature.Name;
+        _sensorFactory = sensorFactory;
+
+        AvailableSensorTypes = _sensorFactory.GetAvailableSensors();
     }
 
     public void RefreshSensors()
     {
-        if (_sensorHandler != null) Sensors = _sensorHandler.RefreshAll();
+        if(_sensorHandler == null) return;
+
+        var positions = ActiveSensors
+            .ToDictionary(s => s.Id, s => (s.XPosition, s.YPosition));
+
+        var latestSensor = _sensorHandler.RefreshAll();
+
+        foreach (var sensor in latestSensor)
+        {
+            if (positions.TryGetValue(sensor.Id, out var pos))
+            {
+                sensor.XPosition = pos.XPosition;
+                sensor.YPosition = pos.YPosition;
+            }
+        }
+        ActiveSensors = new ObservableCollection<ISensorDisplayModel>(latestSensor);
     }
 
-    public void SetSensor()
+    public void SetSensor(string sensorType, double xPosition = 0, double yPosition = 0)
     {
-        // sensor handler in logic to make a new sensor, log it, keep track!!
-        _sensorHandler.CreateSensor();
-        RefreshSensors();
+        var sensor = _sensorHandler.CreateSensor(sensorType);
+
+        var displayModel = sensor.ToDisplayModel();
+        displayModel.XPosition = xPosition;
+        displayModel.YPosition = yPosition;
+        
+        _activeSensors.Add(displayModel);
     }
     
     private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
