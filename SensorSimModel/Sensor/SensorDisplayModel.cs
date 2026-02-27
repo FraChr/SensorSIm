e98bd5412ecb7b95a639c8352ccfb02f3664d52c
FraChr <empynia@gmail.com>
2026-02-27 13:49:53 +0100

    now uses Id have position information and dynamicly updates value based on incoming data from environment

--- SensorSimModel/Sensor/SensorDisplayModel.cs
+++ SensorSimModel/Sensor/SensorDisplayModel.cs
@@ -1,9 +1,44 @@
-﻿using SensorSimDependancies.LogicInterfaces;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SensorSimDependancies.LogicInterfaces;
 
 namespace SensorSimModel.Sensor;
 
-public class SensorDisplayModel : ISensorDisplayModel
+public class SensorDisplayModel : ISensorDisplayModel, INotifyPropertyChanged
 {
+    public string Id { get; }
     public string Name { get; set; }
-    public string Temperature { get; set; }
+    
+    private string _value;
+
+    public string Value
+    {
+        get => _value;
+        set => SetField(ref _value, value);
+    }
+    
+    public double XPosition { get; set; }
+    public double YPosition { get; set; }
+
+
+    public SensorDisplayModel(string id)
+    {
+        Id = id;
+    }
+    
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
 }
