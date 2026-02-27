d9ebe545c4b154bdfa2d84f80fc19a5f6751e8c6
FraChr <empynia@gmail.com>
2026-02-27 13:33:55 +0100

    Added a pressure sensor

---
+++ SensorSimModel/Sensor/PressureSensor.cs
@@ -0,0 +1,27 @@
+﻿using SensorSimDependancies.LogicInterfaces;
+using SensorSimDependancies.ModelInterfaces;
+
+namespace SensorSimModel.Sensor;
+
+public class PressureSensor : SensorBase, ISensorLogic
+{
+    private string Id { get; set; } = Guid.NewGuid().ToString();
+    
+    public string SensorType => "Pressure";
+    public Func<IOcean, double> GetEnvironmentValue { get; } = ocean => ocean.Pressure;
+    private string Name { get; set; } = "PressureSensor";
+    private double Pressure { get; set; }
+    public void Update(double value)
+    {
+        Pressure = value;
+    }
+
+    public ISensorDisplayModel ToDisplayModel()
+    {
+        return new SensorDisplayModel(Id)
+        {
+            Name = Name,
+            Value = $"{Pressure} Bar",
+        };
+    }
+}
