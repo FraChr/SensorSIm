2d135a1627129a99374e9f1b3c84aa43ba6d8493
FraChr <empynia@gmail.com>
2026-03-05 13:13:49 +0100

    helper methods for UI Project

--- SensorSimUI/HelpersUi.cs
+++ SensorSimUI/HelpersUi.cs
@@ -1,6 +1,16 @@
-﻿namespace SensorSimUI;
+﻿using System.Windows.Threading;
 
-public class HelpersUi
+namespace SensorSimUI;
+
+public static class HelpersUi
 {
-    
+    public static DispatcherTimer SetupTick(TimeSpan interval, EventHandler handler)
+    {
+        DispatcherTimer tickTimer = new()
+        {
+            Interval = interval
+        };
+        tickTimer.Tick += handler;
+        return tickTimer;
+    }
 }
