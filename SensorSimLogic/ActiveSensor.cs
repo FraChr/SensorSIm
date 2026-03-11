ddbdbbf28bcc2e2c1b69b03681eff902ca7b0476
FraChr <empynia@gmail.com>
2026-03-11 14:40:30 +0100

    made new object to bind sensor with its display model

---
+++ SensorSimLogic/ActiveSensor.cs
@@ -0,0 +1,9 @@
+﻿using SensorSimModel.Interfaces;
+
+namespace SensorSimLogic;
+
+public class ActiveSensor(ISensor sensor, ISensorDisplayModel display)
+{
+    public ISensor Sensor {get; set;} = sensor;
+    public ISensorDisplayModel Display { get; set; } = display;
+}
