40c60d889c30b3a32c081f4cb43ebe3039db22f9
FraChr <empynia@gmail.com>
2026-03-13 14:13:34 +0100

    Added a converter to convert imagePaths to BitMapImage for wpf

---
+++ SensorSimUI/ImageConverter.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using SensorSimModel.Interfaces;
+
+namespace SensorSimUI;
+
+public class ImageConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        /*if (value is ISensorDisplayModel sensor && !string.IsNullOrEmpty(sensor.SensorImage.ImagePath))
+        {
+            var image = new BitmapImage(new Uri(sensor.SensorImage.ImagePath, UriKind.Relative));
+            image.Freeze();
+            return image;
+        }
+        return null;*/
+        if (value is string path && !string.IsNullOrEmpty(path))
+        {
+            var image = new BitmapImage(new Uri(path, UriKind.Relative));
+            image.Freeze();
+            return image;
+        }
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
