e61046b26359c66d5a706911fee84c0416ca35b2
FraChr <empynia@gmail.com>
2026-03-13 14:11:05 +0100

    Added new resource project with static classes to hold image paths

---
+++ Resources/EnvironmentImagePaths.cs
@@ -0,0 +1,7 @@
+﻿namespace Resources;
+
+public static class ImagePaths
+{
+    public static readonly string OceanBackground = "Assets/Images/Environment/Ocean.png";
+    public static readonly string LakeBackground = "Assets/Images/Environment/Lake.png";
+}
