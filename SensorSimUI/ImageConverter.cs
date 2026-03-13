1264ac2dfedc168053e2a4f1de2f3d6142323678
FraChr <empynia@gmail.com>
2026-03-13 14:27:56 +0100

    removed uncomented code

--- SensorSimUI/ImageConverter.cs
+++ SensorSimUI/ImageConverter.cs
@@ -9,13 +9,6 @@ public class ImageConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        /*if (value is ISensorDisplayModel sensor && !string.IsNullOrEmpty(sensor.SensorImage.ImagePath))
-        {
-            var image = new BitmapImage(new Uri(sensor.SensorImage.ImagePath, UriKind.Relative));
-            image.Freeze();
-            return image;
-        }
-        return null;*/
         if (value is string path && !string.IsNullOrEmpty(path))
         {
             var image = new BitmapImage(new Uri(path, UriKind.Relative));
